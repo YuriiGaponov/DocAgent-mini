23cc334f778dfd300a1d2721b8b63c95aa40fd88
Юрий Гапонов <GREATLORDorel@yandex.ru>
2026-04-11 13:35:25 +0300

    запросы на /ask передаются в модель

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -1,3 +1,5 @@
+from langchain_ollama import ChatOllama
+
 from src.logger import logger
 from src.models import AskRequest
 from src.settings import Settings
@@ -6,9 +8,22 @@ from src.settings import Settings
 class DocAgent:
     def __init__(self, settings: Settings):
         self.settings = settings
+        self._llm = None
         logger.debug('Агент инициализирован')
+
+    @property
+    def llm(self):
+        if self._llm is None:
+            self._llm = ChatOllama(
+                model=self.settings.LLM_MODEL,
+                temperature=self.settings.LLM_TEMPERATURE
+            )
+        logger.trace(f'используется LLM: {self._llm}')
+        return self._llm
 
     async def process_query(self, request_data: AskRequest):
         logger.debug('Запуск DocAgent.process_query')
         logger.trace(f'входящие данные: {request_data}')
-        return request_data
+        request = request_data.query
+        response = await self.llm.ainvoke(request)
+        return response
