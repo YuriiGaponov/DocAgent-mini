851da9b27bdc534d5fe5d8eb587927c0660de06b
Юрий Гапонов <GREATLORDorel@yandex.ru>
2026-04-11 13:14:58 +0300

    инициализация агента

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -1,6 +1,14 @@
+from src.logger import logger
 from src.models import AskRequest
+from src.settings import Settings
 
 
 class DocAgent:
+    def __init__(self, settings: Settings):
+        self.settings = settings
+        logger.debug('Агент инициализирован')
+
     async def process_query(self, request_data: AskRequest):
+        logger.debug('Запуск DocAgent.process_query')
+        logger.trace(f'входящие данные: {request_data}')
         return request_data
