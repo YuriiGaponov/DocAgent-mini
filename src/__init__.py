18b14406b0e3b1001e08e2263364a007088142f5
YuriiGaponov <147429527+YuriiGaponov@users.noreply.github.com>
2026-05-31 19:28:01 +0300

    добавлена админ-панель /admin (#34)

--- src/__init__.py
+++ src/__init__.py
@@ -10,9 +10,10 @@ src.__init__
 отдельных компонентов.
 """
 
+from src.admin import get_admin
 from src.api import router
 from src.db import providerDB
 from src.docs import tags_metadata
 from src.settings import settings
 
-__all__ = ['providerDB', 'router', 'settings', 'tags_metadata']
+__all__ = ['get_admin', 'providerDB', 'router', 'settings', 'tags_metadata']
